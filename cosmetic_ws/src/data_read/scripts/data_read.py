e8502d2fabd8fae561524cbe3c3b7844ebf23574
Kyrrego <zhangyr_linda@163.com>
2023-11-09 18:53:33 +0800

    filter raw dta

--- cosmetic_ws/src/data_read/scripts/data_read.py
+++ cosmetic_ws/src/data_read/scripts/data_read.py
@@ -1,13 +1,69 @@
 import pickle
 import matplotlib.pyplot as plt
 import glob
+import numpy as np
 import os
+from scipy.signal import butter, filtfilt
 
+exp_date = "1028"
 sub_idx = 12
 
-for sub_idx in range(1,13):
-    data_folder = "./data/1028/force_data/subject_{}/".format(sub_idx)
-    result_folder = "./data/1028/result/force_time_graph/subject_{}/".format(sub_idx)
+# Low Pass Filter
+order = 3
+Wn = 1  # cutoff frequency
+fs = 100  # sample rate
+
+
+# Read data from pickle file, plot raw data with opacity and corresponding filtered data
+def plot_data(result_folder, data_file):
+    with open(data_file, "rb") as file:
+        data = pickle.load(file)
+
+    data_np = np.array(data)
+
+    # zeroing the data using the first 100 samples
+    data_np -= np.mean(data_np[:100], axis=0)
+
+    # Low Pass Filter
+    b, a = butter(order, Wn, "lowpass", fs=fs)
+    data_filt = filtfilt(b, a, data_np, axis=0)
+
+    # 创建时间序列（以秒为单位）
+    time_series = np.arange(0, len(data_np)) / fs
+
+    # Plot
+    plt.figure(figsize=(10, 6))
+    raw_lines = plt.plot(time_series, data_np, alpha=0.2, label=["X", "Y", "Z"], linewidth=1)
+    raw_colors = [line.get_color() for line in raw_lines]
+    for i in range(data_filt.shape[1]):
+        plt.plot(time_series, data_filt[:, i], label=f"{i}_filt", color=raw_colors[i])
+
+    # Assuming the Z axis data is the third column in data_filt
+    z_force = data_filt[:, 2]
+
+    # 设置图形标题和标签
+    tag = os.path.basename(data_file).replace(".p", "")
+    plt.title(f"Force Data Over Time, Exp_ID: {tag}")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Force Magnitude (N)")
+    plt.legend()
+
+    # show the plot
+    # plt.show()
+
+    # 构建结果文件名
+    result_filename = os.path.join(result_folder, f"result_{os.path.basename(data_file)}new.pdf")
+
+    # 保存图形为PNG文件
+    plt.savefig(result_filename)
+
+    # 关闭当前图形以准备下一个
+    plt.close()
+
+
+for sub_idx in range(1, 13):
+    data_folder = f"./data/{exp_date}/force_data/subject_{sub_idx}/"
+    result_folder = f"./data/{exp_date}/result/force_time_graph/subject_{sub_idx}/"
 
     # 创建结果文件夹（如果不存在）
     os.makedirs(result_folder, exist_ok=True)
@@ -16,43 +72,8 @@ for sub_idx in range(1,13):
     data_files = glob.glob(os.path.join(data_folder, "*.p"))
 
     for data_file in data_files:
-        with open(data_file, "rb") as file:
-            data = pickle.load(file)
+        plot_data(result_folder, data_file)
+        break
 
-        # 将数据分为x、y和z方向的列表
-        x_data = [item[0] for item in data]
-        y_data = [item[1] for item in data]
-        z_data = [item[2] for item in data]
-
-        # 创建时间序列（以秒为单位）
-        time_series = [i / 100 for i in range(len(data))]  # 将单位设置为秒
-
-        # 绘制x、y和z方向的力大小变化情况
-        plt.figure(figsize=(10, 6))
-        plt.plot(time_series, x_data, label="X Direction")
-        plt.plot(time_series, y_data, label="Y Direction")
-        plt.plot(time_series, z_data, label="Z Direction")
-
-        # 设置图形标题和标签
-        tag = os.path.basename(data_file).replace("_new.p", "")
-        plt.title("Force Data Over Time, Exp_ID: {}".format(tag))
-        plt.xlabel("Time (s)")
-        plt.ylabel("Force Magnitude (N)")
-        plt.legend()
-
-        # show the plot
-        # plt.show()
-
-        # 构建结果文件名
-        result_filename = os.path.join(
-            result_folder, f"result_{os.path.basename(data_file)}.png")
-
-        # 保存图形为PNG文件
-        plt.savefig(result_filename)
-        
-
-
-        # 关闭当前图形以准备下一个
-        plt.close()
-
-    print("图像已保存在{}文件夹中。".format(result_folder))
+    print(f"图像已保存在{result_folder}文件夹中。")
+    break
